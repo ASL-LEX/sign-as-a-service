1929fd2c391bbb808a2f3f020d69bb81f4415663
Justin Chen <ch3njus@gmail.com>
2023-07-24 14:02:27 -0400

    chore: adding back islr demo

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,18 @@
 import uvicorn
+import omegaconf
 import strawberry
 from strawberry.asgi import GraphQL
+from openhands.apis.inference import InferenceModel
 
 @strawberry.type
 class Query:
     @strawberry.field
-    def classify(self, info) -> str:
-        # TODO: Implement classification
-        return "Sign code"
+    def classify(self, info) -> dict:
+        cfg = omegaconf.OmegaConf.load("./config.yaml")
+        model = InferenceModel(cfg=cfg)
+        model.init_from_checkpoint_if_available()
+        model.test_inference()
+
 
 schema = strawberry.Schema(query=Query)
 app = GraphQL(schema)
