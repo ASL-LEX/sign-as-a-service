c15ecf620d32ac1069cb7d650920a932895b9a6c
cbolles <collinbolles@gmail.com>
2023-08-01 12:51:44 -0400

    Setup Gateway

--- packages/gateway/src/app.module.ts
+++ packages/gateway/src/app.module.ts
@@ -1,10 +1,44 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
+import configuration from './config/configuration';
+import { GraphQLModule } from '@nestjs/graphql';
+import { IntrospectAndCompose, RemoteGraphQLDataSource } from '@apollo/gateway';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration]
+    }),
+    GraphQLModule.forRootAsync<ApolloGatewayDriverConfig>({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      driver: ApolloGatewayDriver,
+      useFactory: async (configService: ConfigService) => ({
+        autoSchemaFile: {
+          path: 'schema.gql',
+          federation: 2
+        },
+        gateway: {
+          buildService: ({ url }) => {
+            return new RemoteGraphQLDataSource({
+              url,
+              willSendRequest({ request, context }) {
+                if (context.req && context.req.headers) {
+                  // Copy over authentication
+                  request.http!.headers.set('authorization', context.req.headers.authorization);
+                }
+              }
+            });
+          },
+        },
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            { name: 'lex', url: configService.getOrThrow('lex.uri') }
+          ]
+        })
+      })
+    })
+  ],
 })
 export class AppModule {}
