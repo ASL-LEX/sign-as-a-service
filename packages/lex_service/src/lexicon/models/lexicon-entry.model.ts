34fe3b61fe4e82537cb69aea88c1c554059e507a
Collin Bolles <collinbolles@gmail.com>
2023-08-11 11:05:54 -0400

    feat: Resolve Lexicon Entry #8
    * Ability to resolve lexicon using lexicon ID and key

--- packages/lex_service/src/lexicon/models/lexicon-entry.model.ts
+++ packages/lex_service/src/lexicon/models/lexicon-entry.model.ts
@@ -1,10 +1,11 @@
-import { ObjectType, Field } from '@nestjs/graphql';
+import { ObjectType, Field, Directive } from '@nestjs/graphql';
 import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { Document } from 'mongoose';
 import GraphQLJSON from 'graphql-type-json';
 
 @Schema()
 @ObjectType({ description: 'Single entry within a whole lexicon ' })
+@Directive('@key(fields: "key, lexicon")')
 export class LexiconEntry {
   @Prop({ index: true, unique: true })
   @Field({ description: 'Unique user assigned identifier for the entry within the lexicon' })
