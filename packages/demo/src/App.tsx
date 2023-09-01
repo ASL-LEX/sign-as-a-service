8b3715034947548a60e274e714412fee8df76289
cbolles <collinbolles@gmail.com>
2023-09-01 10:51:54 -0400

    Add basic banner/placeholder

--- packages/demo/src/App.tsx
+++ packages/demo/src/App.tsx
@@ -1,9 +1,11 @@
+import { Banner } from './components/banner.component';
 import { ThemeProvider } from './theme/theme.provider';
+import './App.css';
 
 function App() {
   return (
     <ThemeProvider>
-
+      <Banner />
     </ThemeProvider>
   );
 }
---
+++ packages/demo/src/components/banner.component.tsx
@@ -0,0 +1,31 @@
+import { FC, ReactNode } from 'react';
+import { Box, Grid, Paper, useTheme } from '@mui/material';
+
+export const Banner: FC = () => {
+  const theme = useTheme();
+
+  return (
+    <Box sx={{ flexGrow: 1, backgroundColor: theme.palette.primary.main, padding: 3, margin: 5 }}>
+      <Grid container direction='row' spacing={5} justifyContent='space-around'>
+        <Grid item xs={4}>
+          <InfoCard>
+            Text Placeholder
+          </InfoCard>
+        </Grid>
+        <Grid item xs={4}>
+          <InfoCard>
+            Video Placeholder
+          </InfoCard>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+const InfoCard: FC<{ children: ReactNode }> = ({ children }) => {
+  return (
+    <Paper sx={{ minWidth: 250, minHeight: 250, padding: 1 }} elevation={3}>
+      {children}
+    </Paper>
+  );
+};
