c45e06da666c991b01096838c42641b775602f9c
cbolles <collinbolles@gmail.com>
2023-09-01 12:23:58 -0400

    Working dropdown option

--- packages/view/src/components/DropDown/DropDown.component.tsx
+++ packages/view/src/components/DropDown/DropDown.component.tsx
@@ -1,7 +1,23 @@
-import { FC } from 'react';
+import { Dispatch, FC, SetStateAction } from 'react';
+import { Lexicon } from '../../graphql/graphql';
+import { FormControl, TextField, Autocomplete } from '@mui/material';
 
-export const DropDown: FC = () => {
+export interface DropDownProps {
+  setValue: Dispatch<SetStateAction<Lexicon | null>>;
+  options: Lexicon[];
+};
+
+export const DropDown: FC<DropDownProps> = ({ setValue, options }) => {
   return (
-    <p>Hello World</p>
+    <FormControl>
+      <Autocomplete
+        sx={{ width: 300 }}
+        options={options}
+        getOptionLabel={(lexicon) => lexicon.name}
+        renderInput={(params) => <TextField {...params} label='Lexicon' />}
+        onChange={(_event: any, newValue: Lexicon | null) => setValue(newValue)}
+        isOptionEqualToValue={(option, value) => option._id == value._id}
+      />
+    </FormControl>
   );
 };
--- packages/view/src/components/DropDown/DropDown.stories.tsx
+++ packages/view/src/components/DropDown/DropDown.stories.tsx
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { DropDown } from './DropDown.component';
+import { useState } from 'react';
+import { Lexicon } from '../../graphql/graphql';
 
 const meta: Meta<typeof DropDown> = {
   title: 'DropDown View',
@@ -9,7 +11,19 @@ const meta: Meta<typeof DropDown> = {
 export default meta;
 type Story = StoryObj<typeof DropDown>;
 
-export const Primary: Story = (args: any) => <DropDown {...args} />;
+export const Primary: Story = (args: any) => {
+  const [value, setValue] = useState<Lexicon | null>(null);
+
+  const options: Lexicon[] = [
+    { _id: '1', name: 'ASL-LEX', schema: {}},
+    { _id: '2', name: 'LSE-LEX', schema: {}}
+  ];
+
+  return (
+    <DropDown value={value} setValue={setValue} options={options} {...args} />
+  );
+};
+
 Primary.args = {
 
 };
