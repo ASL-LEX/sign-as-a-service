20d940e5d41d18d99c7462c83a398cdab4049250
cbolles <collinbolles@gmail.com>
2023-10-13 16:13:14 -0400

    Attempt to replicate model results

---
+++ packages/model_service/app.py
@@ -0,0 +1,102 @@
+from collections import OrderedDict
+from recognition.model import r2plus1d_18
+import torch
+import torchvision
+import numpy as np
+import os
+from PIL import Image, ImageOps
+
+
+# TODO: Have these options be configurable
+image_size = 128
+folder_path = '/home/cbolles/devel/temp/saas/CVPR21Chal-SLR/data/test_frames/girl_respo/'
+checkpoint_file = '/home/cbolles/devel/temp/saas/final_models_finetuned/rgb_final_finetuned.pth'
+num_frames = 32
+test_clips = 5
+
+
+def frame_indices_tranform_test(video_length, sample_duration, clip_no=0):
+    """
+    TODO: Assess necessity of the following method
+
+    Taken from
+    https://github.com/jackyjsy/CVPR21Chal-SLR/blob/main/Conv3D/dataset_sign_clip.py#L46
+    """
+    if video_length > sample_duration:
+        start = (video_length - sample_duration) // (test_clips - 1) * clip_no
+        frame_indices = np.arange(start, start + sample_duration) + 1
+    elif video_length == sample_duration:
+        frame_indices = np.arange(sample_duration) + 1
+    else:
+        frame_indices = np.arange(video_length)
+        while frame_indices.shape[0] < sample_duration:
+            frame_indices = np.concatenate((frame_indices, np.arange(video_length)), axis=0)
+        frame_indices = frame_indices[:sample_duration] + 1
+
+    return frame_indices
+
+
+def read_images(folder: str, transform: torchvision.transforms.Compose, clip_no: int) -> torch.Tensor:
+    images = []
+    index_list = frame_indices_tranform_test(len(os.listdir(folder_path)), 32, clip_no)
+    for i in index_list:
+        image = Image.open(os.path.join(folder_path, '{:04d}.jpg').format(i))
+        images.append(transform(image))
+
+    # Convert the images fro a 3D CNN
+    images = torch.stack(images, dim=0)
+    images = images.permute(1, 0, 2, 3)
+    return images
+
+
+def main():
+    # PyTorch setup
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # Load model
+    model = r2plus1d_18(pretrained=True, num_classes=226)
+
+    # Restore model from checkpoint
+    checkpoint = torch.load(checkpoint_file)
+    new_state_dict = OrderedDict()
+    for k, v in checkpoint.items():
+        name = k[7:]
+        new_state_dict[name] = v
+    model.load_state_dict(new_state_dict)
+    model = model.to(device)
+
+
+    # Make data transformer
+    transform = torchvision.transforms.Compose([
+        torchvision.transforms.Resize([image_size, image_size]),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize(mean=[0.5], std=[0.5])
+    ])
+
+    # Read in images
+
+    # TODO: Understand why this is called multiple times
+    images = []
+    for i in range(0, test_clips):
+        images.append(read_images(folder_path, transform, i))
+    images = torch.stack(images, dim=0)
+
+    images = images.reshape((1, *images.size()))
+    images = images.to(device)
+
+    outputs_clips = []
+    for i_clip in range(images.size(1)):
+        inputs = images[:,i_clip,:,:]
+        print(inputs.size())
+        outputs_clips.append(model(inputs))
+        # if isinstance(outputs, list):
+        #     outputs = outputs[0]
+    outputs = torch.mean(torch.stack(outputs_clips, dim=0), dim=0)
+    prediction = torch.max(outputs, 1)[1]
+    print(prediction)
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/model_service/recognition/model.py
@@ -0,0 +1,41 @@
+import torch
+import torchvision
+
+
+def convert_relu_to_swish(model):
+    for child_name, child in model.named_children():
+        if isinstance(child, torch.nn.ReLU):
+            setattr(model, child_name, torch.nn.SiLU(True))
+        else:
+            convert_relu_to_swish(child)
+
+
+class r2plus1d_18(torch.nn.Module):
+    """
+    Model for handling single sign detection. This is taken from the following
+    location
+
+    https://github.com/jackyjsy/CVPR21Chal-SLR/blob/main/Conv3D/models/Conv3D.py
+    """
+    def __init__(self, pretrained=True, num_classes=500, dropout_p=0.5):
+        super(r2plus1d_18, self).__init__()
+
+        self.pretrained = pretrained
+        self.num_classes = num_classes
+
+        model = torchvision.models.video.r2plus1d_18(pretrained=self.pretrained)
+
+        # Delete the last layer
+        modules = list(model.children())[:-1]
+
+        self.r2plus1d_18 = torch.nn.Sequential(*modules)
+
+        convert_relu_to_swish(self.r2plus1d_18)
+        self.fc1 = torch.nn.Linear(model.fc.in_features, self.num_classes)
+        self.dropout = torch.nn.Dropout(dropout_p, inplace=True)
+
+    def forward(self, x):
+        out = self.r2plus1d_18(x)
+        out = out.flatten(1)
+        out = self.dropout(out)
+        return self.fc1(out)
