103f00a4dbb7b20f1ca9ff7a612f86e8789aa084
cbolles <collinbolles@gmail.com>
2024-01-23 15:52:44 -0500

    chore: Add text search as an export

--- packages/view/src/index.ts
+++ packages/view/src/index.ts
@@ -2,3 +2,5 @@ export * from './components/Search/Search.component';
 export * from './components/DropDown/DropDown.component';
 export * from './components/SearchResults/SearchResults.component';
 export * from './components/ModeSelector/ModeSelector.component';
+export * from './components/TextSearch/TextSearch.component';
+export * from './components/VideoUpload/VideoUpload.component';
