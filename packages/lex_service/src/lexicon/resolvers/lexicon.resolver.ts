2dcc90909489e3f30b35ea5ebe8a1760afbe0926
Arav <haka@asbindia.org>
2024-10-16 11:05:48 -0400

    implemented update lexicon entry

--- packages/lex_service/src/lexicon/resolvers/lexicon.resolver.ts
+++ packages/lex_service/src/lexicon/resolvers/lexicon.resolver.ts
@@ -5,6 +5,7 @@ import { LexiconCreatePipe } from '../pipes/lexicon-create.pipe';
 import { LexiconService } from '../services/lexicon.service';
 import { AuthGuard } from '../../auth/auth.guard';
 import { UseGuards } from '@nestjs/common';
+import { Schema as JSONSchema } from 'ajv';
 
 @Resolver(() => Lexicon)
 export class LexiconResolver {
@@ -15,6 +16,20 @@ export class LexiconResolver {
   async lexiconCreate(@Args('lexicon', LexiconCreatePipe) lexicon: LexiconCreate): Promise<Lexicon> {
     return this.lexiconService.create(lexicon);
   }
+
+  @Mutation(() => Lexicon)
+  @UseGuards(AuthGuard)
+  async lexiconUpdate(
+  @Args('id') id: string,
+  @Args('name', { nullable: true }) name?: string,
+  @Args('schema', { nullable: true }) schema?: JSONSchema,
+  ): Promise<Lexicon> {
+  const updatedLexicon = await this.lexiconService.update(id, { name, schema });
+  if (!updatedLexicon) {
+    throw new Error(`Lexicon with id ${id} not found`);
+  }
+  return updatedLexicon;
+}
 
   @Query(() => [Lexicon])
   async lexFindAll(): Promise<Lexicon[]> {
